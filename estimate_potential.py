2b8f05fab52e5c04b51981d3b8a5aa008a5b1346
xgxxx <83908160+xgxxx@users.noreply.github.com>
2022-05-13 14:30:58 +0800

    Update estimate_potential.py

--- estimate_potential.py
+++ estimate_potential.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 
-def estimate_potential(gas_phase_mass_SOA,total_particulate_mass_species_time_SOA):
+def estimate_potential(gas_phase_mass_SOA, total_particulate_mass_species_time_SOA, folder_name):
     # gas_phase_mass_SOA: mass concentration for each component at each time (ug/m3), given in post processing file \
     # (e.g. 6.1203140008331e-07 ug/m3)
     # total_particulate_mass_species_time_SOA: total mass concentration for each component in all size bins \
@@ -26,4 +26,4 @@ def estimate_potential(gas_phase_mass_SOA,total_particulate_mass_species_time_SO
         potential[i] = np.concatenate((potential[i][:1], potential_SOA[i]))
     col_name = np.array([[' '] + [str(i) + ' minute' for i in range(len(total_particulate_mass_species_time_SOA[0])-1)]])
     potential = np.vstack((col_name, potential))
-    pd.DataFrame(potential).to_csv('SOA_potential.csv', index=False, header=False)
+    pd.DataFrame(potential).to_csv('SOA_potential'+folder_name+'.csv', index=False, header=False)
